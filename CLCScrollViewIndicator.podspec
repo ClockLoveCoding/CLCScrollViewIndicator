789c001486d9805ef9e1e215549bb1af88fabdf4
ClockLoveCoding <clock.hu@icloud.com>
2025-09-28 11:54:25 +0800

    Optimize

--- CLCScrollViewIndicator.podspec
+++ CLCScrollViewIndicator.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'CLCScrollViewIndicator'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'Custom scrollView Indicator.'
 
   s.description      = <<-DESC
--- CLCScrollViewIndicator/Classes/TaskCenter/CLCDebounceTask.m
+++ CLCScrollViewIndicator/Classes/TaskCenter/CLCDebounceTask.m
@@ -7,57 +7,86 @@
 //
 
 #import "CLCDebounceTask.h"
+#import <os/lock.h>
 
 static dispatch_queue_t CLCDebounceTaskDefaultQueue;
 
-@interface CLCDebounceTask ()
-
-@property (nonatomic, copy) dispatch_block_t task;
-
+@interface CLCDebounceTask () {
+    os_unfair_lock _lock;
+    uint32_t       _token;
+    dispatch_block_t _holder;
+}
 @end
 
 @implementation CLCDebounceTask
 
-- (void)debounceTask:(void(^)(void))task oncePerDuration:(NSTimeInterval)durationThreshold {
-    [self debounceTask:task onQueue:CLCDebounceTask.defaultQueue oncePerDuration:durationThreshold];
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _lock = OS_UNFAIR_LOCK_INIT;
+        _token = 0;
+        _holder = nil;
+    }
+    return self;
 }
 
-- (void)debounceTask:(void(^)(void))task onQueue:(dispatch_queue_t)queue oncePerDuration:(NSTimeInterval)durationThreshold {
+- (void)debounceTask:(dispatch_block_t)task oncePerDuration:(NSTimeInterval)durationThreshold {
+    [self debounceTask:task onQueue:CLCDebounceTaskDefaultQueue oncePerDuration:durationThreshold];
+}
+
+- (void)debounceTask:(dispatch_block_t)task
+             onQueue:(dispatch_queue_t)queue
+      oncePerDuration:(NSTimeInterval)durationThreshold {
     NSParameterAssert(queue);
     NSParameterAssert(task);
+
     [self cancelTask];
-    @synchronized(self) {
-        self.task = dispatch_block_create(0, task);
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(durationThreshold * NSEC_PER_SEC)), queue, self.task);
-    }
+
+    os_unfair_lock_lock(&_lock);
+    uint32_t thisToken = ++_token;
+    _holder = [task copy];
+    os_unfair_lock_unlock(&_lock);
+
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW,
+                                            (int64_t)(durationThreshold * NSEC_PER_SEC));
+    
+    __weak typeof(self) weakSelf = self;
+    dispatch_after(popTime, queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        if (!strongSelf) return;
+        
+        os_unfair_lock_lock(&(strongSelf->_lock));
+        if (thisToken != strongSelf->_token) {
+            os_unfair_lock_unlock(&(strongSelf->_lock));
+            return;
+        }
+        dispatch_block_t exec = strongSelf->_holder;
+        strongSelf->_holder = nil;
+        os_unfair_lock_unlock(&(strongSelf->_lock));
+        if (exec) exec();
+    });
 }
 
 - (void)cancelTask {
-    @synchronized(self) {
-        if (!self.task) return;
-        dispatch_block_cancel(self.task);
-        self.task = nil;
-    }
+    os_unfair_lock_lock(&_lock);
+    _token++;
+    _holder = nil;
+    os_unfair_lock_unlock(&_lock);
 }
 
 + (void)initialize {
-  if (self == [CLCDebounceTask class]) {
-      CLCDebounceTaskDefaultQueue = dispatch_get_main_queue();
-  }
+    if (self == [CLCDebounceTask class]) {
+        CLCDebounceTaskDefaultQueue = dispatch_get_main_queue();
+    }
 }
 
 + (dispatch_queue_t)defaultQueue {
-  @synchronized(self) {
     return CLCDebounceTaskDefaultQueue;
-  }
 }
 
 + (void)setDefaultQueue:(dispatch_queue_t)queue {
-  NSParameterAssert(queue);
-
-  @synchronized(self) {
-      CLCDebounceTaskDefaultQueue = queue;
-  }
+    NSParameterAssert(queue);
+    CLCDebounceTaskDefaultQueue = queue;
 }
 
 @end
